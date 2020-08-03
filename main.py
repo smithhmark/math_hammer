0d286c3d2cf633baf4863f1a5c500364ceaf4e20
Mark Smith <smith.h.mark@gmail.com>
2020-08-03 10:33:11 -0400

    basic wounds per round

--- main.py
+++ main.py
@@ -54,17 +54,83 @@ def defeat_invuln_probability(invuln):
 def defeat_armor_probability(sv, ap):
   outs = 6 - sv + 1 + ap
   if outs <= 0:
-    return 0.0
+    return 1.0
   else: 
-    return 1 - outs / 6
+    return 1.0 - outs / 6
 
 _expected_damage_table = {
-    "d3": 2,
+    "d3": 2.0,
     "d6": 3.5,
 }
 
-def expected_damage(damage):
+def average_damage(damage):
     if isinstance(damage, str):
         return _expected_damage_table[damage]
     else:
         return damage
+
+def apply_wargear_strength(unit_strength, weapon_strength):
+    if isinstance(weapon_strength, str):
+        if weapon_strength[0] == '+':
+            st = int(weapon_strength[1:])
+            return unit_strength + st
+        else:
+            return int(weapon_strength)
+    else:
+        return unit_strength + weapon_strength
+
+def is_melee(wargear):
+    if "melee" in wargear.get("type", ''):
+        return True
+    else:
+        return False
+
+def get_wargear_attacks(wargear):
+    last_word = wargear['type'].split()[-1]
+    try:
+        attacks = int(last_word)
+        return attacks
+    except TypeError:
+        return 0
+
+def get_attack_stats_base(attacker, wargear):
+    if is_melee(wargear):
+        skill = attacker.get('ws',0)
+        attacks = attacker.get('a', 0)
+    else:
+        skill = attacker.get('bs',0)
+        attacks = get_wargear_attacks(wargear)
+    stats = {
+        "s": apply_wargear_strength(attacker.get('s',0), wargear.get('s', 0)),
+        "damage": int(wargear.get("d", 0)),
+        "attacks": attacks,
+        "skill": skill,
+        "ap": int(wargear.get("ap", 0))
+    }
+    return stats
+
+def get_defender_stats_base(defender):
+    return {
+        "t": defender['t'],
+        'invuln': defender['invuln'],
+        "w": defender['w'],
+        "fnp": defender.get('fnp',0),
+        'sv': defender['sv'],
+    }
+
+def expected_damage(attacking_stats, defending_stats):
+    hit_prob = hit_probability(attacking_stats['skill'])
+    print(f"hit_prob:{hit_prob}")
+    print(attacking_stats)
+    wound_prob = wound_probability(attacking_stats['s'], defending_stats['t'])
+    print(f"wound_prob:{wound_prob}")
+    beats_save = defeat_armor_probability(defending_stats['sv'], attacking_stats['ap'])
+    beats_invuln = defeat_invuln_probability(defending_stats['invuln'])
+    print(f"beats_save:{beats_save}")
+    print(f"beats_invuln:{beats_invuln}")
+    damage = average_damage(attacking_stats['damage'])
+    print(f"damage:{damage}")
+    attacks = attacking_stats['attacks']
+    print(f"attacks:{attacks}")
+
+    return hit_prob * wound_prob * min(beats_save, beats_invuln) * damage * attacks
--- main_test.py
+++ main_test.py
@@ -62,7 +62,18 @@ def galvanic_rifle_wargear_dict():
         's': '4',
         'ap': '0',
         'd': '1',
-        "rules": "Each time you make a wound roll of 6+ for this weapon, that hit is resolved with an AP of -1."
+        "rules": ["Each time you make a wound roll of 6+ for this weapon, that hit is resolved with an AP of -1."]
+    }
+
+@pytest.fixture
+def gauss_blaster_wargear_dict():
+    return {
+        'range': '24',
+        'type': 'Rapid Fire 1',
+        's': '5',
+        'ap': '-2',
+        'd': '1',
+        "rules": [],
     }
     
 @pytest.fixture
@@ -95,6 +106,8 @@ def test_wound_outs():
     assert main.wound_outs(strength, toughness) == 4
     toughness = 1
     assert main.wound_outs(strength, toughness) == 5
+
+    assert main.wound_outs(5, 3) == 4
     
 def test_defeats_save_base():
     sv = 2
@@ -123,18 +136,45 @@ def test_defeat_armor():
     assert_approx_eq(main.defeat_armor_probability(sv, ap), float(5/6))
     sv = 6
     ap = -1
-    assert_approx_eq(main.defeat_armor_probability(sv, ap), 0.0)
+    assert_approx_eq(main.defeat_armor_probability(sv, ap), 1.0)
     sv = 6
     ap = -2
-    assert_approx_eq(main.defeat_armor_probability(sv, ap), 0.0)
+    assert_approx_eq(main.defeat_armor_probability(sv, ap), 1.0)
 
-def test_expected_damage():
+def test_average_damage():
     damage = 1
-    assert main.expected_damage(damage) == 1
+    assert main.average_damage(damage) == 1
     damage = 3
-    assert main.expected_damage(damage) == 3
+    assert main.average_damage(damage) == 3
     damage = "d3"
-    assert main.expected_damage(damage) == 2
+    assert main.average_damage(damage) == 2
     damage = "d6"
-    assert main.expected_damage(damage) == 3.5
+    assert main.average_damage(damage) == 3.5
 
+
+def test_attack_stats(necron_immortal_stats_dict, gauss_blaster_wargear_dict):
+    attacker = necron_immortal_stats_dict
+    using_wargear = gauss_blaster_wargear_dict
+    final_stats = main.get_attack_stats_base(attacker, using_wargear)
+    assert "attacks" in final_stats
+    assert "damage" in final_stats
+    assert "skill" in final_stats
+    assert "ap" in final_stats
+
+    assert final_stats['attacks'] == 1
+    assert final_stats['skill'] == 3
+    assert final_stats['ap'] == -2
+    assert final_stats['damage'] == 1
+
+def test_expected_damage(necron_immortal_stats_dict, gauss_blaster_wargear_dict, skitarii_ranger_stats_dict):
+    attack_stats = main.get_attack_stats_base(necron_immortal_stats_dict, gauss_blaster_wargear_dict)
+    defense_stats = main.get_defender_stats_base(skitarii_ranger_stats_dict)
+    expt_damage = main.expected_damage(attack_stats, defense_stats)
+    assert_approx_eq(expt_damage, 0.37, 0.001)
+
+def test_apply_wargear_strength(necron_immortal_stats_dict, gauss_blaster_wargear_dict):
+    unit_s = necron_immortal_stats_dict['s']
+    wargear_s = gauss_blaster_wargear_dict['s']
+    got = main.apply_wargear_strength(unit_s, wargear_s)
+
+    assert got == 5
